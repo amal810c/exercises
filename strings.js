d8464122745fec8e756b4c6b5951aa3b30b00bc5
amaliegrøn <amal810c@stud.kea.dk>
2021-02-05 13:29:08 +0100

    strings commit

---
+++ strings.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const longline =
+  "A very, very, very very, very,very, very, very very, very, very,\nlong line";
+
+const name = "Amalie";
+const drink = `cola`;
+
+const text = `Hello ${name}, 
+
+            would you like a ${drink}?`;
+
+console.log(text);
