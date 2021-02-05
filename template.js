b3a8c2d83192bed7cb7b6055d66994644794c725
amaliegrøn <amal810c@stud.kea.dk>
2021-02-05 13:39:58 +0100

    template literals done

---
+++ template.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const name = "Lois";
+const animalname = "Brain";
+const animal = "dog";
+
+const text = `My name is ${name}. 
+I have a ${animal} called ${animalname}.`;
+
+console.log(text);
