a7fb3c3df8fc8087c3b2fbcc6444569ebc94ec21
amaliegrøn <amal810c@stud.kea.dk>
2021-02-08 14:40:59 +0100

    capitalization3 html and js

---
+++ capitalization3.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/* 
+//peTer
+const randomname = "peter";
+//Turn randomname into lowercase
+const lowercasename = randomname.toLowerCase();
+const oneuppercase = lowercasename.substring(2, 3).toUpperCase();
+const result =
+  lowercasename.substring(0, 2) + oneuppercase + lowercasename.substring(3, 5);
+console.log(result); */
+/* 
+//PEter
+const randomname = "peter";
+//Turn randomname into lowercase
+const lowercasename = randomname.toLowerCase();
+const twouppercase = lowercasename.substring(0, 2).toUpperCase();
+const result = twouppercase + lowercasename.substring(2, 5);
+console.log(result); */
+
+//pETer
+/* const randomname = "peter";
+//Turn randomname into lowercase
+const lowercasename = randomname.toLowerCase();
+const twouppercase = lowercasename.substring(1, 3).toUpperCase();
+const result =
+  lowercasename.substring(0, 1) + twouppercase + lowercasename.substring(3, 5);
+console.log(result); */
+
+//peteR
+/* const randomname = "peter";
+//Turn randomname into lowercase
+const lowercasename = randomname.toLowerCase();
+const oneuppercase = lowercasename.substring(4, 5).toUpperCase();
+const result = lowercasename.substring(0, 4) + oneuppercase;
+console.log(result); */
+
+//PEtER
+const randomname = "peter";
+//Turn randomname into lowercase
+const uppercasename = randomname.toUpperCase();
+const onelowercase = uppercasename.substring(2, 3).toLowerCase();
+const result =
+  uppercasename.substring(0, 2) + onelowercase + uppercasename.substring(3, 5);
+console.log(result);
