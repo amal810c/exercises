d01f70d98165f6a3c4436370c6ee392da23a89ca
amaliegrøn <amal810c@stud.kea.dk>
2021-02-08 15:03:12 +0100

    capitalization html and js

---
+++ capitalization.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const someName = "amalie";
+const first = someName.substring(0, 1).toUpperCase();
+const rest = someName.substring(1).toLowerCase();
+console.log(`${first}${rest}`);
