e6f6a1e49a36b16a601eb7406e0fa72a3c4fe2dd
amaliegrøn <amal810c@stud.kea.dk>
2021-02-09 17:26:17 +0100

    array.js and array.html

---
+++ array.js
@@ -0,0 +1,34 @@
+"use strict";
+// til arays 5,4
+/* const letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+console.log(letters[4]);
+
+const newarr = letters;
+newarr[4] = "*";
+
+console.log(letters[4]); */
+
+//til arrays 5,5
+//const people = ["Harry", "Ron", "Hermione", "Luna", "Neville"];
+//let result;
+
+//result = people.unshift("Amalie");
+//result = people.indexOf("Fred");
+//people[1] = "Ginny";
+
+//console.log(people);
+//console.log(result);
+/* 
+const str = "abcdefghijklmn";
+const arr1 = str.split();
+const arr2 = Array.from(str);
+
+console.log(arr1);
+console.log(arr2); */
+
+/* const people = ["Harry", "Ron", "Hermione", "Luna", "Neville"];
+console.log(people.toString(" ")); */
+
+const people = ["Harry", "Ron", "Hermione", "Luna", "Neville"];
+console.log(people.join(" "));
