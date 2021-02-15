8e03ecec0aeb9ede3d6ad7fe85c5a63e2de4431d
amaliegrøn <amal810c@stud.kea.dk>
2021-02-15 14:00:40 +0100

    colorconvert hex to rgb

---
+++ colorconvert.js
@@ -0,0 +1,56 @@
+"use strict";
+
+//#c0ffee
+hexToRGB("#c0ffee");
+
+function hexToRGB(string) {
+  //split hex into tree parts and convert to numbers
+  const r = parseInt(string.substring(1, 3), 16);
+  const g = parseInt(string.substring(3, 5), 16);
+  const b = parseInt(string.substring(5, 7), 16);
+  console.log(`${r} ${g} ${b}`);
+}
+
+//#bada55
+hexToRGB("#bada55");
+
+function hexToRGB(string) {
+  //split hex into tree parts and convert to numbers
+  const r = parseInt(string.substring(1, 3), 16);
+  const g = parseInt(string.substring(3, 5), 16);
+  const b = parseInt(string.substring(5, 7), 16);
+  console.log(`${r} ${g} ${b}`);
+}
+
+//#facade
+hexToRGB("#facade");
+
+function hexToRGB(string) {
+  //split hex into tree parts and convert to numbers
+  const r = parseInt(string.substring(1, 3), 16);
+  const g = parseInt(string.substring(3, 5), 16);
+  const b = parseInt(string.substring(5, 7), 16);
+  console.log(`${r} ${g} ${b}`);
+}
+
+//#f00b42
+hexToRGB("#f00b42");
+
+function hexToRGB(string) {
+  //split hex into tree parts and convert to numbers
+  const r = parseInt(string.substring(1, 3), 16);
+  const g = parseInt(string.substring(3, 5), 16);
+  const b = parseInt(string.substring(5, 7), 16);
+  console.log(`${r} ${g} ${b}`);
+}
+
+//#c0de25
+hexToRGB("#c0de25");
+
+function hexToRGB(string) {
+  //split hex into tree parts and convert to numbers
+  const r = parseInt(string.substring(1, 3), 16);
+  const g = parseInt(string.substring(3, 5), 16);
+  const b = parseInt(string.substring(5, 7), 16);
+  console.log(`${r} ${g} ${b}`);
+}
