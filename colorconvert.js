c39e04eb6ac4cea51e8e5dffcde749a3bb47a24f
amaliegrøn <amal810c@stud.kea.dk>
2021-02-15 16:36:25 +0100

    colorconvert rgb to hex

--- colorconvert.js
+++ colorconvert.js
@@ -1,7 +1,8 @@
 "use strict";
 
+//PART 1
 //#c0ffee
-hexToRGB("#c0ffee");
+/* hexToRGB("#c0ffee");
 
 function hexToRGB(string) {
   //split hex into tree parts and convert to numbers
@@ -42,15 +43,219 @@ function hexToRGB(string) {
   const g = parseInt(string.substring(3, 5), 16);
   const b = parseInt(string.substring(5, 7), 16);
   console.log(`${r} ${g} ${b}`);
-}
+} */
 
 //#c0de25
-hexToRGB("#c0de25");
+/* hexToRGB("#c0de25");
 
 function hexToRGB(string) {
   //split hex into tree parts and convert to numbers
+
   const r = parseInt(string.substring(1, 3), 16);
   const g = parseInt(string.substring(3, 5), 16);
   const b = parseInt(string.substring(5, 7), 16);
   console.log(`${r} ${g} ${b}`);
+}
+ */
+
+//PART 2
+
+/* // 186 218 85
+let colorObject = {
+  r: 186,
+  g: 218,
+  b: 85,
+};
+
+rgbToHex(colorObject);
+
+function rgbToHex(colorObject) {
+  let r = colorObject.r.toString(16);
+  let g = colorObject.g.toString(16);
+  let b = colorObject.b.toString(16);
+  if (r.length < 2) {
+    r = "0" + r;
+  }
+  if (g.length < 2) {
+    g = "0" + g;
+  }
+  if (b.length < 2) {
+    b = "0" + b;
+  }
+  console.log(`#${r}${g}${b}`); // = #bada55
+} */
+
+/* // 250 202 222
+let colorObject = {
+  r: 250,
+  g: 202,
+  b: 222,
+};
+
+rgbToHex(colorObject);
+
+function rgbToHex(colorObject) {
+  let r = colorObject.r.toString(16);
+  let g = colorObject.g.toString(16);
+  let b = colorObject.b.toString(16);
+  if (r.length < 2) {
+    r = "0" + r;
+  }
+  if (g.length < 2) {
+    g = "0" + g;
+  }
+  if (b.length < 2) {
+    b = "0" + b;
+  }
+  console.log(`#${r}${g}${b}`); // = #facade
+} */
+/* 
+// 192	255	238
+let colorObject = {
+  r: 192,
+  g: 255,
+  b: 238,
+};
+
+rgbToHex(colorObject);
+
+function rgbToHex(colorObject) {
+  let r = colorObject.r.toString(16);
+  let g = colorObject.g.toString(16);
+  let b = colorObject.b.toString(16);
+  if (r.length < 2) {
+    r = "0" + r;
+  }
+  if (g.length < 2) {
+    g = "0" + g;
+  }
+  if (b.length < 2) {
+    b = "0" + b;
+  }
+  console.log(`#${r}${g}${b}`); // = #c0ffee
+} */
+
+/* // 240	11	66
+let colorObject = {
+  r: 240,
+  g: 11,
+  b: 66,
+};
+
+rgbToHex(colorObject);
+
+function rgbToHex(colorObject) {
+  let r = colorObject.r.toString(16);
+  let g = colorObject.g.toString(16);
+  let b = colorObject.b.toString(16);
+  if (r.length < 2) {
+    r = "0" + r;
+  }
+  if (g.length < 2) {
+    g = "0" + g;
+  }
+  if (b.length < 2) {
+    b = "0" + b;
+  }
+  console.log(`#${r}${g}${b}`); // = #f00b42
+} */
+
+/* // 192	222	37
+let colorObject = {
+  r: 192,
+  g: 222,
+  b: 37,
+};
+
+rgbToHex(colorObject);
+
+function rgbToHex(colorObject) {
+  let r = colorObject.r.toString(16);
+  let g = colorObject.g.toString(16);
+  let b = colorObject.b.toString(16);
+  if (r.length < 2) {
+    r = "0" + r;
+  }
+  if (g.length < 2) {
+    g = "0" + g;
+  }
+  if (b.length < 2) {
+    b = "0" + b;
+  }
+  console.log(`#${r}${g}${b}`); // = #c0de25
+} */
+
+/* // 12	2	17
+let colorObject = {
+  r: 12,
+  g: 2,
+  b: 17,
+};
+
+rgbToHex(colorObject);
+
+function rgbToHex(colorObject) {
+  let r = colorObject.r.toString(16);
+  let g = colorObject.g.toString(16);
+  let b = colorObject.b.toString(16);
+  if (r.length < 2) {
+    r = "0" + r;
+  }
+  if (g.length < 2) {
+    g = "0" + g;
+  }
+  if (b.length < 2) {
+    b = "0" + b;
+  }
+  console.log(`#${r}${g}${b}`); // = #0c0211
+} */
+
+/* // 1	2	3
+let colorObject = {
+  r: 1,
+  g: 2,
+  b: 3,
+};
+
+rgbToHex(colorObject);
+
+function rgbToHex(colorObject) {
+  let r = colorObject.r.toString(16);
+  let g = colorObject.g.toString(16);
+  let b = colorObject.b.toString(16);
+  if (r.length < 2) {
+    r = "0" + r;
+  }
+  if (g.length < 2) {
+    g = "0" + g;
+  }
+  if (b.length < 2) {
+    b = "0" + b;
+  }
+  console.log(`#${r}${g}${b}`); // = #010203
+} */
+
+// 9	0	37
+let colorObject = {
+  r: 9,
+  g: 0,
+  b: 37,
+};
+
+rgbToHex(colorObject);
+
+function rgbToHex(colorObject) {
+  let r = colorObject.r.toString(16);
+  let g = colorObject.g.toString(16);
+  let b = colorObject.b.toString(16);
+  if (r.length < 2) {
+    r = "0" + r;
+  }
+  if (g.length < 2) {
+    g = "0" + g;
+  }
+  if (b.length < 2) {
+    b = "0" + b;
+  }
+  console.log(`#${r}${g}${b}`); // = #090025
 }
